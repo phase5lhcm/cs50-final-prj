d1ec37c8a661f91417ffe111c9a85c456b609d70
Christine Maynard <christine.ri.maynard@gmail.com>
2021-12-30 17:12:11 -0500

    Build User model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,14 @@
+from enum import unique
 from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False,unique=True)
+    email = db.Column(db.String(),nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000) 
+    items = db.relationship('Product', backref='owned_by',lazy=True)
+
 
 class Product(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -6,6 +16,7 @@ class Product(db.Model):
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False,unique=True)
     description = db.Column(db.String(length=1000), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
     
     def __repr__(self):
         return f'Product {self.name}'
